b2ae217c0020c26c1703ef9af4317dd08a5b6807
Loghan Ramassamy <loghan.r@gmail.com>
2019-12-03 16:33:28 +0100

    Use Character instead of String in the map

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -4,23 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Factory {
-    private final Map<String, Integer> warehouseShipDuration;
+    private final Map<Character, Integer> warehouseShipDuration;
 
     public Factory() {
         this.warehouseShipDuration = new HashMap<>();
-        warehouseShipDuration.put("A", 5);
-        warehouseShipDuration.put("B", 4);
+        warehouseShipDuration.put('A', 5);
+        warehouseShipDuration.put('B', 4);
     }
 
     public int shipTo(String warehouses) {
         int totalDuration = 0;
         for (int i = 0; i < warehouses.length(); i++) {
-            totalDuration = shipOneContainerTo(String.valueOf(warehouses.charAt(i)));
+            totalDuration = shipOneContainerTo(warehouses.charAt(i));
         }
         return totalDuration;
     }
 
-    private int shipOneContainerTo(String warehouse) {
+    private int shipOneContainerTo(char warehouse) {
         return warehouseShipDuration.get(warehouse);
     }
 }
