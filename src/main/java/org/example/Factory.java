3d54540b7f3cfe620846b311d465f5194804a7db
Loghan Ramassamy <loghan.r@gmail.com>
2019-12-03 15:58:42 +0100

    Introducing map

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Factory {
+    Map<String, Integer> hm = new HashMap<>();
+
     public int shipTo(String warehouse) {
+        hm.put("A", 5);
+        hm.put("B", 4);
         if ("A".equals(warehouse))
             return 5;
         return 4;
